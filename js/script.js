c77071359bbee6926b3cd3ddfc80b5cfffc57eba
hehehe yesss <artur.in.the.box@yandex.ru>
2018-05-01 01:04:24 +0300

    d
    Signed-off-by: hehehe yesss <artur.in.the.box@yandex.ru>

--- js/script.js
+++ js/script.js
@@ -35,6 +35,8 @@ $(document).ready(function() {
         }
     });
 
+    $('#polygon-circle .inner-circle').css('border-width',($(window).width()*0.35156/100)+'px');
+
     var modalsOpeningSpeed = 300;
     $('.modal-opener').click(function() {
         var button = $(this);
